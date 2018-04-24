8a2e9fe4e7d0b4d72750898aeef8af95f7f7e778
rosariopfernandes <rosariopfernandes>
2018-04-24 18:22:18 +0200

    Initialized CLion Project

---
+++ cmake-build-debug/CMakeFiles/EDA2TrabalhoPratico.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EDA2TrabalhoPratico.dir/main.cpp.o"
+  "EDA2TrabalhoPratico.pdb"
+  "EDA2TrabalhoPratico"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EDA2TrabalhoPratico.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/EDA2TrabalhoPratico.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for EDA2TrabalhoPratico.
+# This may be replaced when dependencies are built.
